9031530e2a5d1c1b8d92a14dce8efecf1b9e1139
Кириллов Кирилл <Kirillow66@gmail.com>
2024-01-31 11:47:28 +0300

    Lesson14 add all

--- lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherCache.java
+++ lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherCache.java
@@ -1,44 +1,50 @@
 package ru.sberbank.jd.lesson14;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import ru.sberbank.jd.lesson14.WeatherInfo.Builder;
 
 /**
- * Weather cache.
+ * Потокобезопасный кеш, который позволяет получить актуальную информацио о погоде. Информация считается актуальной в
+ * течение 5 минут после загрузки из интернета.
  */
 public class WeatherCache {
 
-    private final Map<String, WeatherInfo> cache = new HashMap<>();
-    private final WeatherProvider weatherProvider;
+    private Map<String, WeatherInfo> cache = new HashMap<>();
+    private WeatherProvider weatherProvider;
 
-    /**
-     * Constructor.
-     *
-     * @param weatherProvider - weather provider
-     */
     public WeatherCache(WeatherProvider weatherProvider) {
         this.weatherProvider = weatherProvider;
     }
 
     /**
-     * Get ACTUAL weather info for current city or null if current city not found.
-     * If cache doesn't contain weather info OR contains NOT ACTUAL info then we should download info
-     * If you download weather info then you should set expiry time now() plus 5 minutes.
-     * If you can't download weather info then remove weather info for current city from cache.
+     * Позволяет добавить в кэш загруженную информацию и считать из кэша если информация уже есть.
      *
-     * @param city - city
-     * @return actual weather info
+     * @param city город, по которому запрашивается информация.
+     * @return Информация о запрашиваемом городе.
      */
     public WeatherInfo getWeatherInfo(String city) {
-        // should be implemented
-        return null;
+        synchronized (cache) {
+            WeatherInfo info = cache.get(city);
+            if (info == null || info.getExpiryTime().isBefore(LocalDateTime.now())) {
+                info = weatherProvider.get(city);
+                cache.remove(city);
+                cache.put(city, info);
+            }
+            return info;
+        }
     }
 
     /**
-     * Remove weather info from cache.
-     **/
+     * Метод для удаления информации о городе из кэша.
+     *
+     * @param city название удалённого города
+     */
     public void removeWeatherInfo(String city) {
-        // should be implemented
+        synchronized (cache) {
+            cache.remove(city);
+        }
     }
 }
 
--- lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherProvider.java
+++ lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherProvider.java
@@ -1,21 +1,45 @@
 package ru.sberbank.jd.lesson14;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
+import org.springframework.web.client.RestTemplate;
+
+
 /**
  * Weather provider.
  */
 public class WeatherProvider {
 
-    //private RestTemplate restTemplate = null;
+    private RestTemplate restTemplate = new RestTemplate();
+    private static final String API_KEY = "0c48922c38f8e6e08091504a58613d63"; // Replace with your actual API key
 
     /**
-     * Download ACTUAL weather info from internet.
-     * You should call GET http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-     * You should use Spring Rest Template for calling requests
+     * Download ACTUAL weather info from internet. You should call GET
+     * http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} You should use Spring Rest Template
+     * for calling requests
      *
      * @param city - city
      * @return weather info or null
      */
     public WeatherInfo get(String city) {
-        return null;
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
+        try {
+            String jsonResponse = restTemplate.getForObject(url, String.class);
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setVisibilityChecker(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
+                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                    .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+                    .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+            WeatherInfo.Builder builder = objectMapper.readValue(jsonResponse, WeatherInfo.Builder.class);
+            WeatherInfo info = builder
+                    .expiryTime(LocalDateTime.now().plusMinutes(5))
+                    .build();
+            return info;
+        } catch (Exception e) {
+            System.out.println("Error fetching weather info: " + e.getMessage());
+            return null;
+        }
     }
 }
